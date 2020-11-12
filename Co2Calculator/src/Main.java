81a18661ed1aed8536263eda981881aa57e394e2
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-11-11 23:33:59 -0300

    Main creation.

--- Co2Calculator/src/Main.java
+++ Co2Calculator/src/Main.java
@@ -1,9 +1,10 @@
+import view.DataCollectScreen;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DataCollectScreen teste = new DataCollectScreen();
 	}
 
 }
