5faab9614a71ccf9ea8bb3c4beee7017c42b0471
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-11-13 23:11:03 -0300

    Main upgrade.

--- Co2Calculator/src/Main.java
+++ Co2Calculator/src/Main.java
@@ -1,10 +1,11 @@
+import controller.Controller;
 import view.DataCollectScreen;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		DataCollectScreen teste = new DataCollectScreen();
+		Controller controller = new Controller();
 	}
 
 }
